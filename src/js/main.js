08f11adb43ee5b5989f2e32845e79eca4254fd87
Vanej88 <vanej88@gmail.com>
2021-02-09 17:08:35 +0100

    fetch de options

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,20 @@
 export function main() {
-  fetch("https://dog.ceo/api/breeds/list/all")
-    .then(response => response.json)
-    .then(data => console.log(data))
+
+  let lista = document.querySelector("#selectRazas"); 
   
-}
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then(response => response.json())
+    .then(data => {
+      let dog = Object.keys(data.message);
+      for(let item of dog){
+        let option = document.createElement('option'); 
+        option.setAttribute('class', 'raza-option');
+        option.setAttribute('value', item)
+        option.innerHTML = item; 
+        lista.appendChild(option)
+      }
+      
+     
+    }
+      
+    )}
