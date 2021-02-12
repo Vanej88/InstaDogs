c9fe9a0a3672ce77e70f2bb90fa25ba1a2ba41e6
Vanej88 <vanej88@gmail.com>
2021-02-12 17:47:12 +0100

    corrección en fetch de imágenes

--- src/js/main.js
+++ src/js/main.js
@@ -12,27 +12,36 @@ export function main() {
         option.setAttribute('value', item)
         option.innerHTML = item; 
         lista.appendChild(option)
-
+      
         let buttom = document.querySelector(".hero__btn-primary")
-        buttom.onclick = callback
-  
+        buttom.onclick = callback; 
+        const raza = option.value
+
         function callback(event){
-          const raza = option.value
+    
+          const galeria = document.querySelector('.gallery__box')
           
-          fetch(`https://dog.ceo/api/breed/${raza}/images/3`)
+          fetch(`https://dog.ceo/api/breed/${raza}/images/random/8`)
           .then(response => response.json())
           .then(data => {
             let dogImg = data.message;
             console.log(dogImg)
+            for(let item of dogImg){
               let card = document.createElement("img");
-              card.setAttribute('src', dogImg); 
-              raza.appendChild(card)
+              let contenedorCard = document.createElement("div"); 
+              card.setAttribute('src', item); 
+              //card.setAttribute('class', 'gallery__img');
+              card.classList.add('.gallery__box');
+              card.classList.add('.gallery__img'); 
+              card.classList.add('.container-gallery');
+              galeria.appendChild(card)
+            }
+            
           })
- 
+      
         }
-      }
+  
     }
-  )
-}
+  })}
 
-
+  
