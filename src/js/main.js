a08f598cfb0135305a071dcfa06989c5d8e9a60c
Vanej88 <vanej88@gmail.com>
2021-02-14 13:09:30 +0100

    fetch final de imágenes y cambios en select

--- src/js/main.js
+++ src/js/main.js
@@ -7,41 +7,51 @@ export function main() {
     .then(data => {
       let dog = Object.keys(data.message);
       for(let item of dog){
-        let option = document.createElement('option'); 
-        option.setAttribute('class', 'raza-option');
-        option.setAttribute('value', item)
-        option.innerHTML = item; 
-        lista.appendChild(option)
-      
+          let option = document.createElement('option'); 
+          option.setAttribute('class', 'raza-option');
+          option.setAttribute('value', item)
+          option.innerHTML = item; 
+          lista.appendChild(option)
+        }
+
         let buttom = document.querySelector(".hero__btn-primary")
         buttom.onclick = callback; 
-        const raza = option.value
 
         function callback(event){
     
-          const galeria = document.querySelector('.gallery__box')
-          
+          const raza = lista.value
+          const galeria = document.querySelector('.gallery__container')
+          galeria.innerHTML = ""
+
           fetch(`https://dog.ceo/api/breed/${raza}/images/random/8`)
           .then(response => response.json())
           .then(data => {
             let dogImg = data.message;
             console.log(dogImg)
             for(let item of dogImg){
+              let title = document.querySelector('#galleryTitle'); 
+              let contenedorCol = document.createElement("div"); 
+              let contenedorBoxImg = document.createElement("div"); 
               let card = document.createElement("img");
-              let contenedorCard = document.createElement("div"); 
+
+              title.innerHTML = `Galería de ${raza}`; 
+              contenedorCol.classList.add('gallery__col');
+              contenedorBoxImg.classList.add('gallery__box-img');
+              card.classList.add('gallery__img'); 
               card.setAttribute('src', item); 
-              //card.setAttribute('class', 'gallery__img');
-              card.classList.add('.gallery__box');
-              card.classList.add('.gallery__img'); 
-              card.classList.add('.container-gallery');
-              galeria.appendChild(card)
+              
+              contenedorBoxImg.appendChild(card)
+              contenedorCol.appendChild(contenedorBoxImg)
+              galeria.appendChild(contenedorCol)
+        
             }
+            window.scrollTo(0,window.innerHeight);
             
           })
-      
-        }
+ 
   
     }
-  })}
+  })
+}
 
   
